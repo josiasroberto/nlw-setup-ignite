61811f58756f7bd4bb12ae3c1293d58e14f892b8
Josias Roberto <josiasroberto1@gmail.com>
2023-01-21 14:16:41 -0300

    Aula 02 - React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs'
+
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -9,6 +10,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,27 +27,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-
-      <Text style={styles.text}>-CR7 Júnior: Tenho fome, pai!</Text>
-      <Text style={styles.text}>-CR7: Sou lindo</Text>
-      {/* <StatusBar style="light" /> */}
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#e6d5d5c3',
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    color: "#7C3AED",
-    fontFamily: 'Inter_700Bold'
-  }
-});
